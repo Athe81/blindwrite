c3ed73e54e4cc67c046fd412053e782a27f8629c
Bubblepoint <t.matt81+github@gmail.com>
2016-03-23 23:39:00 +0100

    basic init

---
+++ keyboard.js
@@ -0,0 +1,220 @@
+var keyboards = {
+    "ergodox": {
+        "svg":
+            `
+            <svg id="keyboardSVG" height="81.684mm" viewBox="0 0 825.51069 289.4326" width="232.98mm" version="1.1">
+            <g stroke="#000000">
+            <g id="layer1" stroke-linejoin="round" fill-opacity="0" transform="translate(-38.513 -34.122)" stroke-width="2">
+            <path id="Lkeyboard" style="image-rendering:auto" d="m76.6 42.209c-16.62 0-30 13.38-30 30v140c0 16.62 13.38 30 30 30h113.09l178.89 71.119c15.444 6.1399 32.821-1.3506 38.961-16.795l26.254-66.043c6.1398-15.444-1.3506-32.821-16.795-38.961l-57.188-22.734v-96.586c0-16.62-13.38-30-30-30h-253.21z"/>
+            <rect id="L00" rx="5" ry="5" height="30" width="60" y="57" x="63"/>
+            <rect id="L01" rx="5" ry="5" height="30" width="60" y="93" x="63"/>
+            <rect id="L02" rx="5" ry="5" height="30" width="60" y="129" x="63"/>
+            <rect id="L03" rx="5" ry="5" height="30" width="60" y="165" x="63"/>
+            <rect id="L04" rx="5" ry="5" height="30" width="30" y="201" x="93"/>
+            <rect id="L10" rx="5" ry="5" height="30" width="30" y="57" x="129"/>
+            <rect id="L11" rx="5" ry="5" height="30" width="30" y="93" x="129"/>
+            <rect id="L12" rx="5" ry="5" height="30" width="30" y="129" x="129"/>
+            <rect id="L13" rx="5" ry="5" height="30" width="30" y="165" x="129"/>
+            <rect id="L14" rx="5" ry="5" height="30" width="30" y="201" x="129"/>
+            <rect id="L20" rx="5" ry="5" height="30" width="30" y="57" x="165"/>
+            <rect id="L21" rx="5" ry="5" height="30" width="30" y="93" x="165"/>
+            <rect id="L22" rx="5" ry="5" height="30" width="30" y="129" x="165"/>
+            <rect id="L23" rx="5" ry="5" height="30" width="30" y="165" x="165"/>
+            <rect id="L24" rx="5" ry="5" height="30" width="30" y="201" x="165"/>
+            <rect id="L30" rx="5" ry="5" height="30" width="30" y="57" x="201"/>
+            <rect id="L31" rx="5" ry="5" height="30" width="30" y="93" x="201"/>
+            <rect id="L32" rx="5" ry="5" height="30" width="30" y="129" x="201"/>
+            <rect id="L33" rx="5" ry="5" height="30" width="30" y="165" x="201"/>
+            <rect id="L34" rx="5" ry="5" height="30" width="30" y="201" x="201"/>
+            <rect id="L40" rx="5" ry="5" height="30" width="30" y="57" x="237"/>
+            <rect id="L41" rx="5" ry="5" height="30" width="30" y="93" x="237"/>
+            <rect id="L42" rx="5" ry="5" height="30" width="30" y="129" x="237"/>
+            <rect id="L43" rx="5" ry="5" height="30" width="30" y="165" x="237"/>
+            <rect id="L44" rx="5" ry="5" height="30" width="30" y="201" x="237"/>
+            <rect id="L50" rx="5" ry="5" height="30" width="30" y="57" x="273"/>
+            <rect id="L51" rx="5" ry="5" height="30" width="30" y="93" x="273"/>
+            <rect id="L52" rx="5" ry="5" height="30" width="30" y="129" x="273"/>
+            <rect id="L53" rx="5" ry="5" height="30" width="30" y="165" x="273"/>
+            <rect id="L60" rx="5" ry="5" height="30" width="30" y="57" x="309"/>
+            <rect id="L61" rx="5" ry="5" height="48" width="30" y="93" x="309"/>
+            <rect id="L62" rx="5" ry="5" height="48" width="30" y="147" x="309"/>
+            <rect id="L63" transform="rotate(21.68)" rx="5" ry="5" height="66" width="30" y="76" x="362"/>
+            <rect id="L70" transform="rotate(21.68)" rx="5" ry="5" height="30" width="30" y="40" x="398"/>
+            <rect id="L71" transform="rotate(21.68)" rx="5" ry="5" height="66" width="30" y="76" x="398"/>
+            <rect id="L80" transform="rotate(21.68)" rx="5" ry="5" height="30" width="30" y="40" x="434"/>
+            <rect id="L81" transform="rotate(21.68)" rx="5" ry="5" height="30" width="30" y="76" x="434"/>
+            <rect id="L82" transform="rotate(21.68)" rx="5" ry="5" height="30" width="30" y="112" x="434"/>
+            <path id="Rkeyboard" style="image-rendering:auto" d="m825.94 42.209c16.62 0 30 13.38 30 30v140c0 16.62-13.38 30-30 30h-113.09l-178.89 71.119c-15.444 6.1399-32.821-1.3506-38.961-16.795l-26.254-66.043c-6.1398-15.444 1.3506-32.821 16.795-38.961l57.188-22.734v-96.586c0-16.62 13.38-30 30-30h253.21z"/>
+            <rect id="R60" rx="5" ry="5" height="30" width="30" y="57" x="563"/>
+            <rect id="R61" rx="5" ry="5" height="48" width="30" y="93" x="563"/>
+            <rect id="R62" rx="5" ry="5" height="48" width="30" y="147" x="563"/>
+            <rect id="R50" rx="5" ry="5" height="30" width="30" y="57" x="599"/>
+            <rect id="R51" rx="5" ry="5" height="30" width="30" y="93" x="599"/>
+            <rect id="R52" rx="5" ry="5" height="30" width="30" y="129" x="599"/>
+            <rect id="R53" rx="5" ry="5" height="30" width="30" y="165" x="599"/>
+            <rect id="R40" rx="5" ry="5" height="30" width="30" y="57" x="635"/>
+            <rect id="R41" rx="5" ry="5" height="30" width="30" y="93" x="635"/>
+            <rect id="R42" rx="5" ry="5" height="30" width="30" y="129" x="635"/>
+            <rect id="R43" rx="5" ry="5" height="30" width="30" y="165" x="635"/>
+            <rect id="R44" rx="5" ry="5" height="30" width="30" y="201" x="635"/>
+            <rect id="R30" rx="5" ry="5" height="30" width="30" y="57" x="671"/>
+            <rect id="R31" rx="5" ry="5" height="30" width="30" y="93" x="671"/>
+            <rect id="R32" rx="5" ry="5" height="30" width="30" y="129" x="671"/>
+            <rect id="R33" rx="5" ry="5" height="30" width="30" y="165" x="671"/>
+            <rect id="R34" rx="5" ry="5" height="30" width="30" y="201" x="671"/>
+            <rect id="R20" rx="5" ry="5" height="30" width="30" y="57" x="707"/>
+            <rect id="R21" rx="5" ry="5" height="30" width="30" y="93" x="707"/>
+            <rect id="R22" rx="5" ry="5" height="30" width="30" y="129" x="707"/>
+            <rect id="R23" rx="5" ry="5" height="30" width="30" y="165" x="707"/>
+            <rect id="R24" rx="5" ry="5" height="30" width="30" y="201" x="707"/>
+            <rect id="R10" rx="5" ry="5" height="30" width="30" y="57" x="743"/>
+            <rect id="R11" rx="5" ry="5" height="30" width="30" y="93" x="743"/>
+            <rect id="R12" rx="5" ry="5" height="30" width="30" y="129" x="743"/>
+            <rect id="R13" rx="5" ry="5" height="30" width="30" y="165" x="743"/>
+            <rect id="R14" rx="5" ry="5" height="30" width="30" y="201" x="743"/>
+            <rect id="R00" rx="5" ry="5" height="30" width="60" y="57" x="779"/>
+            <rect id="R01" rx="5" ry="5" height="30" width="60" y="93" x="779"/>
+            <rect id="R02" rx="5" ry="5" height="30" width="60" y="129" x="779"/>
+            <rect id="R03" rx="5" ry="5" height="30" width="60" y="165" x="779"/>
+            <rect id="R04" rx="5" ry="5" height="30" width="30" y="201" x="779"/>
+            <rect id="R80" ry="5" rx="5" transform="matrix(-.92926 .36943 .36943 .92926 0 0)" height="30" width="30" y="373" x="-404"/>
+            <rect id="R81" ry="5" rx="5" transform="matrix(-.92926 .36943 .36943 .92926 0 0)" height="30" width="30" y="409" x="-404"/>
+            <rect id="R82" ry="5" rx="5" transform="matrix(-.92926 .36943 .36943 .92926 0 0)" height="30" width="30" y="445" x="-404"/>
+            <rect id="R70" ry="5" rx="5" transform="matrix(-.92926 .36943 .36943 .92926 0 0)" height="30" width="30" y="373" x="-440"/>
+            <rect id="R71" ry="5" rx="5" transform="matrix(-.92926 .36943 .36943 .92926 0 0)" height="66" width="30" y="409" x="-440"/>
+            <rect id="R63" ry="5" rx="5" transform="matrix(-.92926 .36943 .36943 .92926 0 0)" height="66" width="30" y="409" x="-476"/>
+            </g>
+            <path id="path4298" d="m205.58 119.83 15.87-0.0838" stroke-width="1px" fill="none"/>
+            <path id="path4298-0" d="m603.51 119.09 15.87-0.0838" stroke-width="1px" fill="none"/>
+            </g>
+            </svg>
+            `,
+        "ordering": [
+                "R42",
+                "L42",
+                "R32",
+                "L32",
+                "R22",
+                "L22",
+                "R12",
+                "L12",
+                "R52",
+                "L52",
+                "R41",
+                "L41",
+                "R41",
+                "R31",
+                "L31",
+                "R21",
+                "L21",
+                "R11",
+                "L11",
+                "R51",
+                "L51",
+                "R43",
+                "L43",
+                "R33",
+                "L33",
+                "R23",
+                "L23",
+                "R13",
+                "L13",
+                "R53",
+                "L53",
+                "R40",
+                "L40",
+                "R40",
+                "R30",
+                "L30",
+                "R20",
+                "L20",
+                "R10",
+                "L10",
+                "R50",
+                "L50",
+                "R44",
+                "L44",
+                "R34",
+                "L34",
+                "R24",
+                "L24",
+                "R14",
+                "L14",
+                "R63",
+                "L63",
+                "R71",
+                "L71",
+                "R00",
+                "L00",
+                "R01",
+                "L01",
+                "R02",
+                "L02",
+                "R03",
+                "L03",
+                "R04",
+                "L04",
+                "R60",
+                "L60",
+                "R61",
+                "L61",
+                "R62",
+                "L62",
+                "R70",
+                "L70",
+                "R80",
+                "L80",
+                "R81",
+                "L81",
+                "R82",
+                "L82"
+            ],
+        "layout": {
+            "ergodox-ez default (en_US)": {
+                "language": "en_US",
+                "mapping": {
+                    " ": "L63",
+                    ",": "L14",
+                    "-": "R00",
+                    ".": "R33",
+                    "0": "R10",
+                    "1": "L10",
+                    "2": "L20",
+                    "3": "L30",
+                    "4": "L40",
+                    "5": "L50",
+                    "6": "R50",
+                    "7": "R40",
+                    "8": "R30",
+                    "9": "R20",
+                    "a": "L12",
+                    "b": "L53",
+                    "c": "L33",
+                    "d": "L32",
+                    "e": "L31",
+                    "f": "L42",
+                    "g": "L52",
+                    "h": "R52",
+                    "i": "R31",
+                    "j": "R42",
+                    "k": "R32",
+                    "l": "R22",
+                    "m": "R43",
+                    "n": "R53",
+                    "o": "R21",
+                    "p": "R11",
+                    "q": "L11",
+                    "r": "L41",
+                    "s": "L22",
+                    "t": "L51",
+                    "u": "R41",
+                    "v": "L43",
+                    "w": "L21",
+                    "x": "L23",
+                    "z": "L13",
+                    "y": "R51"
+                }
+            }
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,296 @@
+"use strict";
+const TXT_LENGTH = 50;
+const HINT_DELAY = 1500;
+
+var hint = function() {
+    var storedKeyDOM, timer;
+
+    var reset = function() {
+        window.clearTimeout(timer);
+        if (typeof(storedKeyDOM) !== "undefined") {
+            storedKeyDOM.setAttribute("fill-opacity", 0);
+        }
+    };
+
+    var set =  function(keyDOM) {
+        reset();
+        storedKeyDOM = keyDOM;
+        timer = window.setTimeout(function() {
+            storedKeyDOM.setAttribute("fill", "#336633");
+            storedKeyDOM.setAttribute("fill-opacity", 1);
+        }, HINT_DELAY);
+    };
+
+    return {
+        reset, 
+        set
+    };
+}();
+
+function exercise() {
+    var lessonString, pos, error, timeStamp;
+    var keyLogging = {};
+    var rankedKeys = JSON.parse(localStorage.getItem("rankedKeys"));
+    var orderdKeys = JSON.parse(localStorage.getItem("orderdKeys"));
+
+    var txtDone = document.getElementById('txtDone');
+    var txtNow = document.getElementById('txtNow');
+    var txtNext = document.getElementById('txtNext');
+
+    document.getElementById("exercise").classList.remove("hide");
+    keyboard.setKeyboard(localStorage.keyboard);
+    newLesson();
+
+    window.onkeypress = function(event){
+        checkInput(event);
+    };
+
+    function updateView() {
+        txtDone.innerHTML = lessonString.substring(0, pos);
+        txtNow.innerHTML = lessonString[pos] || "";
+        txtNow.style.background = "";
+        txtNext.innerHTML = lessonString.substring(pos+1, TXT_LENGTH);
+
+        if (pos === 0) {
+            var buffer = "";
+            for (var i = 0; i < rankedKeys.length; i++) {
+                buffer += '<span class="keycapMini" title="Points: ';
+                buffer += rankedKeys[i].points;
+                buffer += '">';
+                buffer += rankedKeys[i].key;
+                buffer += '</span>';
+            }
+            document.getElementById("keysRanked").innerHTML = buffer;
+            buffer = "";
+            for (var i = rankedKeys.length; i < orderdKeys.length; i++) {
+                buffer += '<span class="keycapMini">';
+                buffer += orderdKeys[i];
+                buffer += '</span>';
+            }
+            document.getElementById("keysNext").innerHTML = buffer;
+        }
+    };
+
+    function newLesson() {
+        var space = 4 + Math.floor(Math.random() * 4);
+        if (rankedKeys[rankedKeys.length-1].points > 1500 &&
+            rankedKeys[rankedKeys.length-1].count > 40
+           ) {
+               rankedKeys.push({
+                   "key": orderdKeys[rankedKeys.length],
+                   "points": 0,
+                   "count": 0
+               });
+           }
+
+        keyLogging = {};
+        lessonString = "";
+        pos = 0;
+        for ( var i = 0; i < TXT_LENGTH; i++ ) {
+            if (i === space && i < TXT_LENGTH-2) {
+                lessonString += " ";
+                space += 4 + Math.floor(Math.random() * 4);
+                continue;
+            }
+            var keyPos = Math.floor(Math.random() * rankedKeys.length);
+            lessonString += orderdKeys[keyPos];
+        }
+        updateView();
+        hint.set(keyboard.getDOM(lessonString[pos]));
+    };
+
+    function checkInput(event) {
+        if (lessonString[pos] === String.fromCharCode(event.which)) {
+            var timeStampEnd = new Date().getTime();
+            if (pos !== 0) {
+                var key = lessonString[pos];
+                if (typeof(keyLogging[key]) === "undefined") {
+                    keyLogging[key] = {count: 0, time: 0, err: 0};
+                }
+                keyLogging[key].count++;
+                keyLogging[key].time += timeStampEnd - timeStamp;
+                keyLogging[key].err += error;
+            }
+            pos++;
+            if (pos < TXT_LENGTH) {
+                hint.set(keyboard.getDOM(lessonString[pos]));
+            } else {
+                calculatePoints();
+                hint.reset();
+                newLesson();
+            }
+            updateView();
+            timeStamp = new Date().getTime();
+            error = false;
+        } else {
+            txtNow.style.background = "#996666";
+            error = true;
+        }
+    };
+
+    function calculatePoints() {
+        for (var r in rankedKeys) {
+            var k = rankedKeys[r].key;
+            var speed = 2000 - (keyLogging[k].time / keyLogging[k].count);
+            if (speed < 0) {speed = 0};
+            var errors = (1 - (keyLogging[k].err / keyLogging[k].count));
+            var points = (speed*errors);
+            points += rankedKeys[r].points * 2;
+            points /= 3;
+            rankedKeys[r].points = points.toFixed(2);
+            rankedKeys[r].count += keyLogging[k].count;
+        }
+        rankedKeys.sort(function(a, b){return b.points - a.points});
+        console.log(rankedKeys);
+        localStorage.setItem("rankedKeys", JSON.stringify(rankedKeys));
+    };
+};
+
+var keyboard = function(){
+    var keyDOMs = {};
+
+    function setKeyboard(){
+        document.getElementById("keyboard").innerHTML =
+            keyboards[localStorage.keyboard]["svg"];
+        var layout = keyboards[localStorage.keyboard]["layout"][localStorage.layout]["mapping"];
+        for (var l in layout) {
+            keyDOMs[l] = document.getElementById(layout[l]);
+        }
+    };
+    
+    function getDOM(key) {
+        return keyDOMs[key];
+    };
+
+    return {
+        setKeyboard,
+        getDOM
+    };
+}();
+
+function setup() {
+    var setup = document.getElementById("setup");
+    var saveConfig = document.getElementById("saveConfig");
+    var selectKeyboard = document.getElementById("selectKeyboard");
+
+    setup.classList.remove("hide");
+    updateKeyboards();
+
+    function updateKeyboards() {
+        for (var k in keyboards) {
+            var option = document.createElement("option");
+            option.text = k;
+            selectKeyboard.add(option);
+        }
+        selectKeyboard.selectedIndex = -1;
+        selectKeyboard.onchange = function(event) {
+            updateLayout(event.target.value);
+            showKeyboardPreview(event.target.value);
+        };
+    };
+
+    function showKeyboardPreview(keyboardType) {
+        document.getElementById("preview").innerHTML = keyboards[keyboardType]["svg"]
+    }
+
+    function updateLayout(keyboardType) {
+        var selectLayout = document.getElementById("selectLayout");
+        selectLayout.innerHTML = "";
+        for (var l in keyboards[keyboardType].layout) {
+            var option = document.createElement("option");
+            option.text = l;
+            selectLayout.add(option);
+        }
+        selectLayout.selectedIndex = -1;
+    };
+
+    function createOrder() {
+        var orderdKeys = [];
+        var layout = keyboards[localStorage.keyboard]["layout"][localStorage.layout];
+        var order = keyboards[localStorage.keyboard]["ordering"];
+        for (var o = 0; o < order.length; o++) {
+            for (var l in layout["mapping"]) {
+                if (layout["mapping"][l] === order[o] &&
+                    language[layout["language"]].indexOf(l) !== -1
+                   ) {
+                       orderdKeys.push(l);
+                   }
+            }
+        }
+        localStorage.setItem("orderdKeys", JSON.stringify(orderdKeys));
+        var rankedKeys = [];
+        for (var i = 0; i < 4; i++) {
+            rankedKeys.push({
+                "key": orderdKeys[i],
+                "points": 0,
+                "count": 0
+            });
+        }
+        localStorage.setItem("rankedKeys", JSON.stringify(rankedKeys));
+    };
+
+    function save() {
+        if (
+            selectKeyboard.selectedIndex === -1 ||
+            selectLayout.selectedIndex === -1
+        ) {
+            alert("You have to select a keyboard design and a layout");
+            return;
+        };
+
+        localStorage.keyboard = selectKeyboard.value;
+        localStorage.layout = selectLayout.value;
+        createOrder();
+        setup.classList.add("hide");
+        exercise();
+    };
+
+    saveConfig.onclick = function() {
+        save();
+    };
+    saveConfig.onkeydown = function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            save();
+        };
+    };
+};
+
+function showWelcome() {
+    var welcome = document.getElementById("welcome");
+    var startSetup = document.getElementById("startSetup");
+
+    welcome.classList.remove("hide");
+    startSetup.onclick = function() {
+        welcome.classList.add("hide");
+        setup();
+    };
+    startSetup.onkeydown = function(event) {
+        if (event.keyCode === 13) {
+            welcome.classList.add("hide");
+            setup();
+        };
+    };
+};
+
+function init() {
+    //window.localStorage.clear();
+
+    // drop backspace
+    document.addEventListener("keydown", function(event) {
+        if (event.which === 8) {
+           event.preventDefault();
+        }
+    });
+
+    if (
+        typeof(localStorage.keyboard) === "undefined" ||
+        typeof(localStorage.layout) === "undefined" ||
+        typeof(localStorage.orderdKeys) === "undefined" ||
+        typeof(localStorage.rankedKeys) === "undefined"
+    ) {
+        showWelcome();
+    } else {
+        exercise();
+    }
+}
